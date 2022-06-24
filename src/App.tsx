dd09f992cadd6ef5bfe62a599b39d5d3c5402b5f
Gianluca Ferreira <gianluca.camposferreira@gmail.com>
2022-06-24 20:12:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
+import { client } from "./services/apollo";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App;
